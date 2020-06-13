9f582c0d0dc28adb42db1f13622aa4e09a9841f7
Chunghee Hwang <hch0821@gmail.com>
2020-06-13 23:18:56 +0900

    Update GraphqlApplication.java

--- src/main/java/com/goodperson/graphql/GraphqlApplication.java
+++ src/main/java/com/goodperson/graphql/GraphqlApplication.java
@@ -17,16 +17,13 @@ public class GraphqlApplication {
 		SpringApplication.run(GraphqlApplication.class, args);
 	}
 
-	@Bean
-	ApplicationRunner init(PostService postservice) {
-		return args -> {
-			Stream.of("title1", "title2", "title3", "title4", "title5", "title6").forEach(title -> {
-				Post post = new Post();
-				post.setTitle(title);
-				postservice.savePost(post);
-			});
-			postservice.getPosts().forEach(System.out::println);
-		};
-	}
+	// 더미 데이타 생성
+	/*
+	 * @Bean ApplicationRunner init(PostService postservice) { return args -> {
+	 * Stream.of("title1", "title2", "title3", "title4", "title5",
+	 * "title6").forEach(title -> { Post post = new Post(); post.setTitle(title);
+	 * postservice.savePost(post); });
+	 * postservice.getPosts().forEach(System.out::println); }; }
+	 */
 
 }
